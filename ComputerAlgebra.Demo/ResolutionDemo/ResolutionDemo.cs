054f797f37966091e5d5c0d09daa779cc8a746ad
Johann Dirry <movgp0@gmail.com>
2014-03-21 20:09:27 +0100

    Cleaned up ResolutionDemo

--- ComputerAlgebra.Demo/ResolutionDemo/ResolutionDemo.cs
+++ ComputerAlgebra.Demo/ResolutionDemo/ResolutionDemo.cs
@@ -15,22 +15,18 @@ namespace ResolutionDemo
 {
     class ResolutionDemo : LogicExpressions
     {
-        delegate CABoolean del2(int x, int y);
-        delegate CABoolean del4(int x, int y, int z, int u);
-
         static void Main()
         {
             //Type two clauses in SNF without quantifiers
-            Expression<del4> exp = (x, y, z, u) => !P(x, y) | Q(z, g(u)) | R(z, f(u));
-            Expression<del2> gypotesis = (x, u) => !R(x, f(u)) | P(b, h(a));
+            Expression<Func<int,int,int,int,CABoolean>> exp = (x, y, z, u) => !P(x, y) | Q(z, g(u)) | R(z, f(u));
+            Expression<Func<int, int, CABoolean>> gypotesis = (x, u) => !R(x, f(u)) | P(b, h(a));
 
             var expNode = Expressions2LogicTree.Parse(exp);
             var gypotesisNode = Expressions2LogicTree.Parse(gypotesis);
-            Console.WriteLine("First clause: " + expNode);
-            Console.WriteLine("Second clause: " + gypotesisNode);
+            Console.WriteLine("First clause: {0}", expNode);
+            Console.WriteLine("Second clause: {0}", gypotesisNode);
             var result = ComputerAlgebra.Resolve(expNode, gypotesisNode).ToList();
-            Console.WriteLine("Possible resolvents: ["+string.Join(" ; ", result) + "]");
-            Console.ReadKey();
+            Console.WriteLine("Possible resolvents: [{0}]", string.Join(" ; ", result));
         }
     }
 }
